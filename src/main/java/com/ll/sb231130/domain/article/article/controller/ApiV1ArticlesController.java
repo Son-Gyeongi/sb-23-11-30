1f5cbbceda9e1596ca6ce9717af8141b6c20f795
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-02 00:46:58 +0900

    step 10, 각 엔드포인트 당 ResponseBody 클래스를 두는 것이 좋다.

--- src/main/java/com/ll/sb231130/domain/article/article/controller/ApiV1ArticlesController.java
+++ src/main/java/com/ll/sb231130/domain/article/article/controller/ApiV1ArticlesController.java
@@ -1,13 +1,16 @@
 package com.ll.sb231130.domain.article.article.controller;
 
 import com.ll.sb231130.domain.article.article.dto.ArticleDto;
+import com.ll.sb231130.domain.article.article.entity.Article;
 import com.ll.sb231130.domain.article.article.service.ArticleService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/api/v1/articles")
@@ -16,13 +19,24 @@ import java.util.List;
 public class ApiV1ArticlesController {
     private final ArticleService articleService;
 
+    // 각 엔드포인트 당 ResponseBody 클래스를 두는 것이 좋다.
+    @Getter
+    public static class GetArticlesResponseBody {
+        private final List<ArticleDto> items;
+        private final Map pagination;
+
+        public GetArticlesResponseBody(List<Article> articles) {
+            items = articles
+                    .stream()
+                    .map(ArticleDto::new)
+                    .toList();
+
+            pagination = Map.of("page", 1);
+        }
+    }
+
     @GetMapping("")
-    public List<ArticleDto> getArticles() {
-        // 엔티티를 API 로 그대로 노출하는 것은 좋지 않다. API 규격에 맞춰진 DTO를 사용하자
-        return articleService
-                .findAll()
-                .stream()
-                .map(article -> new ArticleDto(article))
-                .toList();
+    public GetArticlesResponseBody getArticles() {
+        return new GetArticlesResponseBody(articleService.findAllByOrderByIdDesc());
     }
 }
