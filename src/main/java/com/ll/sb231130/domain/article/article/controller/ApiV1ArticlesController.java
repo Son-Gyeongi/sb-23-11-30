469b5768e3ce29f2f54051ba4cb4af30f7e58def
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-02 00:58:22 +0900

    step 11, 모든 엔드포인트의 응답본문에 일정한 양식을 넣어두자.

--- src/main/java/com/ll/sb231130/domain/article/article/controller/ApiV1ArticlesController.java
+++ src/main/java/com/ll/sb231130/domain/article/article/controller/ApiV1ArticlesController.java
@@ -3,16 +3,17 @@ package com.ll.sb231130.domain.article.article.controller;
 import com.ll.sb231130.domain.article.article.dto.ArticleDto;
 import com.ll.sb231130.domain.article.article.entity.Article;
 import com.ll.sb231130.domain.article.article.service.ArticleService;
+import com.ll.sb231130.global.rsData.RsData;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/articles")
 // API 버저닝(V1)을 하는 이유는, API 버전업을 해도 기존 클라이언트가 잘 작동하게 하기 위해서
 @RequiredArgsConstructor
@@ -36,7 +37,8 @@ public class ApiV1ArticlesController {
     }
 
     @GetMapping("")
-    public GetArticlesResponseBody getArticles() {
-        return new GetArticlesResponseBody(articleService.findAllByOrderByIdDesc());
+    public RsData<GetArticlesResponseBody> getArticles() {
+        // 모든 엔드포인트의 응답본문에 일정한 양식을 넣어두자.
+        return RsData.of("200", "성공", new GetArticlesResponseBody(articleService.findAllByOrderByIdDesc()));
     }
 }
