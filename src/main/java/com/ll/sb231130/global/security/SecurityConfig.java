ccef7654b2b0df9624b113fb0cb2d55230657928
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-01 23:55:53 +0900

    step 3, SecurityFilterChain

---
+++ src/main/java/com/ll/sb231130/global/security/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.ll.sb231130.global.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(
+                        authorizeHttpRequests -> authorizeHttpRequests
+                                .requestMatchers(
+                                        // 정적자원에 접근에 대한 속도향상을 위해서 우선적으로 시큐리티 룰에서 허용, h2-console 접속할 수 있도록 함
+                                        PathRequest.toStaticResources().atCommonLocations(), // html, css, js 통과
+                                        new AntPathRequestMatcher("/resources/**"),
+                                        new AntPathRequestMatcher("/h2-console/**")
+                                ).permitAll()
+                                .requestMatchers("/adm/**")
+                                .hasRole("ADMIN")
+                                .anyRequest()
+                                .permitAll()
+                )
+                .headers(
+                        headers -> headers
+                                .addHeaderWriter(
+                                        new XFrameOptionsHeaderWriter(
+                                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN
+                                        )
+                                )
+                )
+                .csrf(
+                        csrf -> csrf.ignoringRequestMatchers("/h2-console/**")
+                )
+                .formLogin(
+                        formLogin -> formLogin
+                                .loginPage("/member/login")
+                )
+                .logout(
+                        logout -> logout.logoutRequestMatcher(
+                                new AntPathRequestMatcher("/member/logout")
+                        )
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
