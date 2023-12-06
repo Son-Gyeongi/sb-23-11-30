56a6c88052aed0d98edf76467678efc8165c856c
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-06 22:18:36 +0900

    work4, 모든 api 요청(/api/**)이 처리되기전에 작동하는 필터 JwtAuthenticationFilter 도입

--- src/main/java/com/ll/sb231130/global/security/ApiSecurityConfig.java
+++ src/main/java/com/ll/sb231130/global/security/ApiSecurityConfig.java
@@ -7,17 +7,21 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity // 메소드 수준의 보안을 활성화
 @RequiredArgsConstructor
 public class ApiSecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
     @Bean
     SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         http
                 .securityMatcher("/api/**")
-                .csrf(AbstractHttpConfigurer::disable);
-        // /api/** 경로로 들어오는 건 csrf토큰 요청 꺼놓는다.
+                .csrf(AbstractHttpConfigurer::disable)
+                // /api/** 경로로 들어오는 건 csrf토큰 요청 꺼놓는다.
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                // 모든 api 요청(/api/**)이 처리되기전에 작동하는 필터 JwtAuthenticationFilter 도입
 
         return http.build();
     }
