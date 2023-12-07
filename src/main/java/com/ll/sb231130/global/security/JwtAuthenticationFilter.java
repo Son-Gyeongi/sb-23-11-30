2b16634fdeb2a2300a727f36ef0c8d9f6f94f539
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-07 21:20:59 +0900

    step 2, 패스워드까지 체크하여 클라이언트가 쉽게 신원을 위조할 수 없도록 한다.

--- src/main/java/com/ll/sb231130/global/security/JwtAuthenticationFilter.java
+++ src/main/java/com/ll/sb231130/global/security/JwtAuthenticationFilter.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -22,6 +23,7 @@ import java.util.List;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     //  Spring Security를 사용하여 JWT(JSON Web Token) 인증을 처리하는 필터
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     @SneakyThrows
@@ -29,12 +31,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
         // 요청에서 "username" 파라미터를 추출합니다.
         String username = request.getParameter("username");
+        // 패스워드까지 체크하여 클라이언트가 쉽게 신원을 위조할 수 없도록 한다.
+        String password = request.getParameter("password");
 
         // 어쩔 수 없이 요청에서 username 파라미터를 통해서 보낸 사람이 누군지 판단
-        // 만약 username이 존재한다면,
-        if (username != null) {
+        // 만약 username, password가 존재한다면,
+        if (username != null && password != null) {
             // MemberService를 사용하여 해당 username을 가진 Member 엔터티를 찾습니다.
             Member member = memberService.findByUsername(username).get();
+
+            // 비밀번호 체크
+            if (!passwordEncoder.matches(password, member.getPassword())) {
+                throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+            }
 
             // 찾은 Member 정보를 기반으로 Spring Security의 User 객체를 생성합니다.
             User user = new User(
